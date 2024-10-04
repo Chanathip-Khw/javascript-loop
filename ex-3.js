1d2c4f7a29ff247862a107b1440df7c378cb24d9
Oat Chana <chanathip0944729842@gmail.com>
2024-10-04 13:23:44 +0700

    feat:add answer loop assignment

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,12 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
 let minScore;
-// Start coding here
+
+minScore = scores[0]
+for (let score of scores) {
+    if (score <= minScore) {
+        minScore = score
+    }
+}
 
 console.log(minScore);
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,8 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
+let number = 1
+for (let letter of companyName) {
+    console.log(`Number ${number} character is ${letter}`)
+    number += 1
+}
 
-// Start coding here
